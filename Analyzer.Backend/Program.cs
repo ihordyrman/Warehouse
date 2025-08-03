551fc05c379593466252d6b784953b8937ed5a81
Ihor <ihor.dyrman@outlook.com>
2025-08-03 22:09:58 +0300

    Initial commit

---
+++ Analyzer.Backend/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/hello", () => { return "world"; })
+    .WithName("Get Hello World");
+
+app.Run();
