9c53c2e92907bd8eec3c22de37fd423e4a9a15be
Ihor <ihor.dyrman@outlook.com>
2025-09-13 19:20:17 +0300

    EventBus implementation

---
+++ Warehouse.Backend/Core/Application/EventBus/EventBusServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Warehouse.Backend.Core.Abstractions.EventBus;
+
+namespace Warehouse.Backend.Core.Application.EventBus;
+
+public static class EventBusServiceCollectionExtensions
+{
+    public static IServiceCollection AddInMemoryEventBus(this IServiceCollection services)
+    {
+        services.AddSingleton<IEventBus, InMemoryEventBus>();
+        return services;
+    }
+
+    public static IServiceCollection AddEventHandler<TEvent, THandler>(this IServiceCollection services)
+        where TEvent : class
+        where THandler : class, IEventHandler<TEvent>
+    {
+        services.AddScoped<THandler>();
+        services.AddScoped<IEventHandler<TEvent>, THandler>();
+        return services;
+    }
+}
---
+++ Warehouse.Backend/Core/Application/EventBus/EventEnvelope.cs
@@ -0,0 +1,10 @@
+﻿namespace Warehouse.Backend.Core.Application.EventBus;
+
+public class EventEnvelope(object eventData, Type eventType, DateTime timestamp)
+{
+    public object EventData { get; } = eventData;
+
+    public Type EventType { get; } = eventType;
+
+    public DateTime Timestamp { get; } = timestamp;
+}
